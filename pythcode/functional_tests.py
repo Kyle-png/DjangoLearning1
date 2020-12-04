9dca18f59be2dc953c6b0fcdea6838db68ddcbe7
Kyle-png <56518508+Kyle-png@users.noreply.github.com>
2020-12-04 01:26:36 -0500

    Front page HTML now generated from a template

--- pythcode/functional_tests.py
+++ pythcode/functional_tests.py
@@ -30,7 +30,7 @@ class NewVistorTest(unittest.TestCase):
 
         # He types "Obtain eye of newt" into the text box
         # (Merlin's hobby is magic)
-        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys('Obtain eye of newt')
 
         # When he presses enter, the page updates, and it now lists the item
         # "1: Obtain eye of newt" as an item in a to-do list
@@ -40,7 +40,8 @@ class NewVistorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Obtain eye of newt' for row in rows),
+            "New to-do item did not appear in table"
         )
 
         # There is still a text box avaliable for use.
