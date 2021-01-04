3aae5883354d2e02cd7c331f7645dbee13bf46ff
Kyle-png <56518508+Kyle-png@users.noreply.github.com>
2021-01-04 11:23:37 -0500

    Commit 3

--- functional_tests.py
+++ functional_tests.py
@@ -5,6 +5,9 @@ from django.test import LiveServerTestCase
 #import unittest
 from selenium.common.exceptions import WebDriverException
 
+# from django.test import TestCase
+# from lists.models import Item, List
+
 MAX_WAIT = 10  
 
 #class NewVistorTest(unittest.TestCase):
@@ -155,9 +158,54 @@ class NewVistorTest(LiveServerTestCase):
             delta=10
         )
 
+    def test_cannot_add_empty_list_items(self):    
+        # Edith goes to the home page and accidentally tries to submit    
+        # an empty list item. She hits Enter on the empty input box    
+        self.browser.get(self.live_server_url)    
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+
+        # The home page refreshes, and there is an error message saying    
+        # # that list items cannot be blank    
+        self.wait_for(lambda: self.assertEqual(        
+            self.browser.find_element_by_css_selector('.has-error').text,        
+            "You can't have an empty list item"    ))    
+        
+        # She tries again with some text for the item, which now works    
+        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')    
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)    
+        self.wait_for_row_in_list_table('1: Buy milk')    
+        
+        # Perversely, she now decides to submit a second blank list item    
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)    
+        
+        # She receives a similar warning on the list page    
+        self.wait_for(lambda: self.assertEqual(        
+            self.browser.find_element_by_css_selector('.has-error').text,        
+            "You can't have an empty list item"    ))    
+
+        # And she can correct it by filling some text in    
+        self.browser.find_element_by_id('id_new_item').send_keys('Make tea')    
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)    
+        self.wait_for_row_in_list_table('1: Buy milk')    
+        self.wait_for_row_in_list_table('2: Make tea')  
+
+        # And she can correct it by filling some text in    
+        self.fail('finish this test!')
+
     # She starts a new list and sees the input is nicely
     # centered there too
     
+    class base(LiveServerTestCase):
+            def wait_for(self, fn):          
+                start_time = time.time()        
+                while True:            
+                    try:                
+                        return fn()            
+                        return            
+                    except (AssertionError, WebDriverException) as e:                
+                        if time.time() - start_time > MAX_WAIT:                    
+                            raise e                
+                        time.sleep(0.5)
 
 #if __name__ == '__main__':
 #    unittest.main(warnings='ignore')
